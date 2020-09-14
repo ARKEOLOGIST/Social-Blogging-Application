8d19616f70c25cee9343214a47e1a379b7d72961
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2020-09-14 20:28:35 +0530

    Made changes to login and menu components, added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '',component: HomeComponent},
+  {path: '',redirectTo: 'home',pathMatch: 'full'},
+  {path: 'home',component: HomeComponent},
   {path: 'login',component: LoginComponent},
-  {path: 'signup',component: SignupComponent},
   {path: '**',redirectTo: 'home'}
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, Form } from '@angular/forms';
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   message: string = "";
   userError: any;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(public fb: FormBuilder, public router: Router, public authService: AuthService) { 
     this.myForm = this.fb.group({
       email: ['',[Validators.email, Validators.required]],
       password: ['', [Validators.required]]
@@ -22,9 +22,10 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form) {
-    firebase.auth().signInWithEmailAndPassword(form.value.email,form.value.password).then((res) => {
+    this.authService.login(form.value.email,form.value.password).then((res) => {
       console.log(res);
       this.message = "You have been logged in successfully";
+      this.router.navigate(['home']);
     }).catch((error) => {
       console.log(error);
       this.userError = error;
